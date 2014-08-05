b1988cbae1045544b7619635d4afb56012455860
John Mitchell <jlmitch5@ncsu.edu>
2014-08-04 22:06:05 -0400

    adding django apps, adding requirements.txt and setup.py dependencies

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ setup(name='Sample django 1.6 on Red Hat Openshift',
     author='Your Name',
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django>=1.6', 'Django<1.7'],
+    install_requires=['Django>=1.6', 'Django<1.7'
+    'django-widget-tweaks>=1.3,<1.4',
+    'djangorestframework>=2.3,<2.4',
+    'requests>=2.3,<2.4',
+    'coverage>=3.7,<3.8',],
 )
